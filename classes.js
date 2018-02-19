867aff9f59c2d31eb9da0769bd54892b4ffa2474
Mark Sprott <30487657+mark-sprott@users.noreply.github.com>
2018-02-19 22:49:23 +1300

    classes.js upload

---
+++ classes.js
@@ -0,0 +1,38 @@
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
+// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+document.addEventListener('DOMContentLoaded', start)
+
+function start () {
+  // The first example is done for you. Uncomment the line below and reload the browser.
+one()
+two()
+three()
+makeVisible()
+  // Your turn! Create a new function called `two`, then call it from here.
+}
+
+function one () {
+  // First, we have to find the element:
+  var one = document.getElementById('one')
+
+  // Next, we apply a new CSS class to it:
+  one.classList.add('blue')
+}
+
+// CREATE FUNCTION two HERE
+function two() {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
+// CREATE FUNCTION three HERE
+function three() {
+  var three = document.getElementById('three')
+  three.classList.add('black')
+}
+// CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var four = document.getElementsByClassName('invisible')
+  four[0].classList.add('visible')
+
+}
